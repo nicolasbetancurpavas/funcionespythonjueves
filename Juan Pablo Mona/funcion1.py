b2128b0aa7fc487d25b33a6718cfd64b5cbeaf2c
308 <308@academicos.cesde.edu.co>
2022-09-22 07:33:37 -0500

    NTJ08 - Se crear funcion con bucles

--- Juan Pablo Mona/funcion1.py
+++ Juan Pablo Mona/funcion1.py
@@ -2,9 +2,29 @@
 
 #Declarando funciones en python
 
+from unittest import result
+
+
 def saludar(nombre):
     print(f'Hola {nombre}')
     
 # Utilizar una función
     
-saludar('Juan Pablo')
+# saludar('Juan Pablo')
+
+def calcular():
+    datos = []
+    i = 0
+    while (i <= 20):
+        dato = int(input("Porfavor ingrese un dato: "))
+        datos.insert(i,dato)
+        i = i + 1
+    temperatura = 0
+    for dato in datos:
+        temperatura = dato + temperatura
+    
+    resultado = temperatura / 20
+    
+    print(resultado)
+
+calcular()
