f2c9f7332b0e6e686a36afa27e8b7443934c1c01
308 <308@academicos.cesde.edu.co>
2022-09-22 07:34:42 -0500

    inicio ejercicio

---
+++ carlosu/funcion1.py
@@ -0,0 +1,9 @@
+#El valle de aburra afronta altas temperaturas año tras año, Cree una que permita calcular la temperatura media de la tierra partir de recibir 20 datos diarios de temperatura.
+
+def temperatura(numeros):
+    
+    temperaturamenor=numeros[0]
+    
+    temperaturamayor=numeros[0]
+    
+    
