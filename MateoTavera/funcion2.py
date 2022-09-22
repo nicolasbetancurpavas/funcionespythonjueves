0e30f5887d8ab41bcb21d74e71ed644dce95f11b
Matiiu <mateotaveraramirez@gmail.com>
2022-09-22 08:13:17 -0500

    Se crea función para medir la media de la temperatura

--- MateoTavera/funcion2.py
+++ MateoTavera/funcion2.py
@@ -2,11 +2,13 @@
 def calcularTemperatura():    
     datos = 2
     dato = 0
+    
     while dato < datos:
         dato += 1
         temperatura = int(input(f"Ingrese el {dato} dato: "))
-        datemperaturato += temperatura
-    print(f"La media de la temperatura es {temperatura}")
+        temperatura += temperatura
+    media = temperatura/dato
+    print(f"La media de la temperatura es {media}")
 
 calcularTemperatura()    
 
