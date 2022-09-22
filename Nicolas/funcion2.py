2140370a577201e7ee045656f232748803822757
308 <308@academicos.cesde.edu.co>
2022-09-22 07:33:03 -0500

    V2 funciones Nico

---
+++ Nicolas/funcion2.py
@@ -0,0 +1,19 @@
+from array import array
+
+
+def calcular(num):
+    array = []
+    cont = 0
+    while(num != 0):
+        temp = int(input("digitar temperatura"))
+        array.append(temp)
+        cont += 1
+        if(cont == 2):
+            contador = 0
+            for i in array:
+                contador= contador + i
+                promedio = contador / 2
+            print(promedio)
+            break
+                
+calcular(1)
