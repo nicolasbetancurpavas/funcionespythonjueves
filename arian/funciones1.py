0d27a4c19abbe2ee9c0a436e7cd3360cf40fb1b5
308 <308@academicos.cesde.edu.co>
2022-09-22 07:34:59 -0500

    se crea funcion mayor y menor

--- arian/funciones1.py
+++ arian/funciones1.py
@@ -1,1 +1,20 @@
 #FUNCIONES EN PYTHON
+
+def min_max(numeros):
+    
+    menor = numeros[0]
+    mayor = numeros[0]
+    
+    for n in numeros:
+        if n < menor:
+            menor = n
+            
+        if n > mayor:
+            mayor = n
+    
+    return menor, mayor
+
+
+datos = [1,2,3,5,4,97,4,21,6,4,12,8,5,64,11,66,20,36,47,57]
+
+print(min_max(datos))
